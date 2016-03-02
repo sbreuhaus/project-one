9ceb6db8844a54c17335a4a02e80db306bc52301
Alexandra Breuhaus <sbreuhaus@gmail.com>
2016-03-02 17:10:20 -0500

    save

---
+++ app.js
@@ -0,0 +1,16 @@
+console.log("JS Loaded");
+
+var xtile = document.querySelector('#x-img');
+xtile.addEventListener('click', function(){
+  console.log("X tile clicked");
+})
+
+var ytile = document.querySelector('#y-img');
+ytile.addEventListener('click', function(){
+  console.log("Y tile clicked");
+})
+
+var ztile = document.querySelector('#z-img');
+ztile.addEventListener('click', function(){
+  console.log("Z tile clicked");
+})
