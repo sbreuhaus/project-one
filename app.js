b3132c92a849b24065156c26acc1b7c76dfd58b9
Alexandra Breuhaus <sbreuhaus@gmail.com>
2016-03-02 19:55:42 -0500

    trivia save

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
 console.log("JS Loaded");
 
-var xtile = document.querySelector('#x-img');
-xtile.addEventListener('click', function(){
+var xtile = document.querySelector('#x-img'); //clickable tile.  Should be able to
+xtile.addEventListener('click', function(){   // add a style to make tiles fade away.
+  xtile.classList.toggle('hidden');
+  ytile.classList.toggle('hidden');
+  ztile.classList.toggle('hidden');
   console.log("X tile clicked");
 })
 
-var ytile = document.querySelector('#y-img');
-ytile.addEventListener('click', function(){
+var ytile = document.querySelector('#y-img'); //clickable tile
+ytile.addEventListener('click', function(){  // add a style to make tiles fade away.
+  xtile.classList.toggle('hidden');
+  ytile.classList.toggle('hidden');
+  ztile.classList.toggle('hidden');
   console.log("Y tile clicked");
 })
 
-var ztile = document.querySelector('#z-img');
-ztile.addEventListener('click', function(){
+var ztile = document.querySelector('#z-img'); //clickable tile
+ztile.addEventListener('click', function(){  // add a style to make tile fade away.
+  xtile.classList.toggle('hidden');
+  ytile.classList.toggle('hidden');
+  ztile.classList.toggle('hidden');
   console.log("Z tile clicked");
 })
+
+var siteWrapperDiv = document.querySelector("site-wrapper");
+
+console.log(siteWrapperDiv);
