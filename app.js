293659e2b73aaa0552122d69ce5b724694cc40a5
Alexandra Breuhaus <sbreuhaus@gmail.com>
2016-03-03 12:52:05 -0500

    remove start tiles

--- app.js
+++ app.js
@@ -3,25 +3,27 @@ console.log("JS Loaded");
 var xtile = document.querySelector('#x-img'); //clickable tile.  Should be able to
 xtile.addEventListener('click', function(){   // add a style to make tiles fade away.
   tileFade();
-  countDownDelay;
+  countDownDelay();
   console.log("X tile clicked");
 })
 
 var ytile = document.querySelector('#y-img'); //clickable tile
 ytile.addEventListener('click', function(){  // add a style to make tiles fade away.
   tileFade();
-  countDownDelay;
+  countDownDelay();
   console.log("Y tile clicked");
 })
 
 var ztile = document.querySelector('#z-img'); //clickable tile
 ztile.addEventListener('click', function(){  // add a style to make tile fade away.
   tileFade();
-  countDownDelay;
+  countDownDelay();
   console.log("Z tile clicked");
 })
 
-var siteWrapperDiv = document.querySelector("site-wrapper");
+var siteWrapperDiv = document.querySelector("#site-wrapper");
+var startTiles = document.querySelectorAll(".start-col");
+
 
 // console.log(siteWrapperDiv);
 
@@ -32,12 +34,20 @@ function tileFade(){
 }
 
 
-
-var countDownDelay = setInterval(function(){
+function countDownDelay(){
+   var delay = setInterval(function(){
    console.log("dlayed")
+   console.log(startTiles);
+  //  startTiles.classList.remove("start-col");
+   siteWrapperDiv.remove(startTiles);
    var countDownDiv = document.createElement('div');
    document.body.appendChild(countDownDiv);
-   countDownDiv.innerText = "hello"
-   countDownDiv.setAttribute("style", "width: 300px; height: 300px; background: red"  )
-   clearInterval(countDownDelay);
-}, 3000)
+   countDownDiv.innerText = "";
+   countDownDiv.setAttribute("style", "width: 100%; height: 100vh; background: yellow; margin: 0;");
+  // countDownDiv.classList.add("#countDown-style");
+   clearInterval(delay);
+}, 1500)
+}
+
+
+// element.classList.remove()
